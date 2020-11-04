ffb0f86de749054af59bdd877f87ba0a7d39ae37
Tinh Van <vanducthanhtinh@gmail.com>
2020-11-04 23:21:37 +0800

    Check negative number

--- main_v1.py
+++ main_v1.py
@@ -1,6 +1,7 @@
 import os
+import sys
 
-#function convert number from Decimal to Binary
+#function convert number from Decimal to Binary. Ex: 3 -> "00000011"
 def decimalToBinary(num):  
     binary_string = bin(num).replace("0b", "")  
     #if len of binary string < 8 --> fill '0' on the left to get full 8 bits
@@ -12,9 +13,12 @@ def decimalToBinary(num):
 def binaryToDecimal(binary_string):
     return int(binary_string,2) 
 
-# def convertNumberListToString(data):
-
+#function to calculate broadcask base on ip and submaks
 def calcBroadcast(ip_binary_string, submask_bit_octet_string):
+    '''
+    Input: IP bit string (Ex: "10010110.10010110.10010110.00000010"), Submask bit string (Ex: " 11111111.11111111.11111111.10000000")
+    Output: broadcast address (Ex: 150.150.150.127), broadcast bit address (Ex: "10010110.10010110.10010110.01111111")
+    '''
     #Calculate broadcast address
     #1. invert all bit of submask
     inv_submask_string = ""
@@ -26,7 +30,7 @@ def calcBroadcast(ip_binary_string, submask_bit_octet_string):
         else:
             inv_submask_string += "."
     
-    #2. OR  invert submask with IP address (binary)
+    #2. method "OR" between invert submask with IP address (binary)
     broadcast_binary = ""
     for i in range(len(inv_submask_string)):
         if inv_submask_string[i] == ".":
@@ -35,6 +39,7 @@ def calcBroadcast(ip_binary_string, submask_bit_octet_string):
             broadcast_binary += "0"
         else:
             broadcast_binary += "1"
+
     #convert broadcast binary to decimal
     broadcast_string_list = []
     broadcast_split = broadcast_binary.split(".")
@@ -45,10 +50,20 @@ def calcBroadcast(ip_binary_string, submask_bit_octet_string):
     broadcast_string = ".".join(broadcast_string_list)
     return broadcast_string, broadcast_binary
 
-
+#Input IP address and check if IP valid
 def inputIP():      #return IP and IP in binary string
+    '''
+    Input: IP string (Ex: 192.168.1.1)
+    Output: ip (string, ex: 192.168.1.1), 
+            ip_number_list (ex: [192,168,1,1])
+            ip_bit_string  (ex: "11000000.10101000.00000001.00000001")
+    Press C (or c) to exit program
+    '''
     while True:
-        ip = input("Input IP address: ")
+        ip = input("Input IP address (Press C to exit): ")
+        if ip.lower() == "c":
+            print("Exit program")
+            sys.exit(0)
         #print("IP: {}".format(ip))
         #Split IP address
         ip_split = ip.split(".")
@@ -58,6 +73,16 @@ def inputIP():      #return IP and IP in binary string
             print("Invalid IP, length of the list must be 4 octets. Try again")
             continue
         
+        #check the IP address can not begin with "0"
+        if ip_split[0] == "0":
+            print("IP address can not begin with 0. Try again")
+            continue
+
+        #check the last octet IP address can not be 0
+        if ip_split[-1] == "0":
+            print("The last octet of IP address can not be 0. Try again")
+            continue
+
         #convert IP string to list of int number
         ip_number_list = []     #list of number of IP octet
         ip_bit_list = []        #list of binary of IP octet
@@ -65,13 +90,13 @@ def inputIP():      #return IP and IP in binary string
         for i in range(len(ip_split)):
             try:
                 ip_int = int(ip_split[i])
-                #check if octet greater than 255
-                if ip_int>=255:
-                    print("Octet can not be greater than 255")
+                #check if any octet greater than 255
+                if ip_int>=255 or ip_int<0:
+                    print("Octet can not be greater than 255 or smaller than 0")
                     isInvalid = True
                     continue
                 ip_number_list.append(ip_int)
-                binary = decimalToBinary(ip_int)
+                binary = decimalToBinary(ip_int)        #convert octet number to binary (Ex: 2 -> "00000010")
                 ip_bit_list.append(binary)
             except:
                 isInvalid = True
@@ -92,19 +117,30 @@ def inputIP():      #return IP and IP in binary string
             print("Invalid IP. IP can not be loopback adress")
             continue
 
-        break
-    #print("Valid IP {}".format(ip))
+        break   #all above condition is valid --> get valid IP, exit loop
+    
     ip_bit_string = ".".join(ip_bit_list)
     return ip, ip_number_list, ip_bit_string
 
-#STEP 2: Enter valid submask
+#Input SUBMASK address and check if submask valid
 def inputSubmask():
     '''
     Input Submask
-    Output: submask (string), submask number list, submask binary string
+    Output: submask (string, Ex: 255.255.255.0), 
+            submask number list (ex: [255,255,255,0]), 
+            submask binary string (Ex: "11111111.11111111.11111111.00000000")
     '''
     while True:
-        submask = input("Input submask: ")
+        submask = input("Input submask (Press C to exit): ")
+        if submask.lower() == "c":
+            print("Exit program")
+            sys.exit(0)
+
+        #check subnet mask 255.255.255.255 is not valid
+        if submask == "255.255.255.255":
+            print("Invalid submask. Submask 255.255.255.255 is not valid. Try again")
+            continue
+
         #split submask
         submask_string_list = submask.split(".")
         #check if submask include 4 octets
@@ -123,17 +159,18 @@ def inputSubmask():
         isInvalid = False
         for i in range(len(submask_string_list)):
             try:
-                submask_int = int(submask_string_list[i])
+                submask_int = int(submask_string_list[i])       #convert string to int
                 #check if octet greater than 255
-                if submask_int>255:
-                    print("Octet can not be greater than 255")
+                if submask_int>255 or submask_int<0:
+                    print("Octet can not be greater than 255 or smaller than 0")
                     isInvalid = True
                     continue
                 submask_number_list.append(submask_int)
+                
                 #convert octet decimal to binary
                 binary = decimalToBinary(submask_int)
                 bit_octet_list.append(binary)
-            except:
+            except: #if can not convert string -> int mean the input is not valid number (for example character)
                 isInvalid = True
                 break
         if isInvalid == True:
@@ -147,7 +184,8 @@ def inputSubmask():
         if "01" in submask_bit_octet_string or "0.1" in submask_bit_octet_string:
             print("Invalid subnet. All network bit must be in the left")
             continue
-        break
+
+        break   #all submask checking condition valid --> get valid submask, exit loop
     return submask, submask_number_list, submask_bit_octet_string
 
        
@@ -181,28 +219,20 @@ if __name__ == "__main__":
     wildcard_bin_string = ".".join(wildcard_bin_list) 
 
     #STEP 6: Determine the network address
-    #change last octet of IP address to 0
+    #change last octet of IP address to 0 (192.168.1.1 --> 192.168.1.0)
     network_address_list = ip_number_list.copy()
     network_address_list[-1] = 0
-    network_address_binary = ip_binary_string[:-8]
+    network_address_binary = ip_binary_string[:-8]      #get IP binary string except last 8 bit and change last 8 bits to "00000000"
     network_address_binary += "00000000"
     #convert network address to string
     network_address_string_list = list(map(str, network_address_list))
     network_address_string = ".".join(network_address_string_list)
 
     #calculate broadcast address
-    
-    # #change last octet of IP address to 1
-    # broadcast_address_list = ip_number_list.copy()
-    # broadcast_address_list[-1] = 1
-    # broadcast_address_binary = ip_binary_string[:-8]
-    # broadcast_address_binary += "11111111"
-    # #convert broadcast address to string
-    # broadcast_address_string_list = list(map(str, broadcast_address_list))
-    # broadcast_address_string = ".".join(broadcast_address_string_list)
     broadcast_string, broadcast_binary = calcBroadcast(ip_binary_string, submask_bit_octet_string)
 
     #STEP 3: OUTPUT
+    print("\n")
     print("IP address:        {}     {}".format(ip, ip_binary_string))
     print("Subnet Netmask:    {}     {}".format(submask, submask_bit_octet_string))
     print("Network address:   {}     {}".format(network_address_string, network_address_binary))
